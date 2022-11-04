6d42da1b835f09a52af36c01aa04ea23130c4adc
Chris Bradly <christof.bradly@gmail.com>
2022-11-04 15:23:32 +1300

    pad rework & magic factor

--- src/TranscorrelatedPotential.jl
+++ src/TranscorrelatedPotential.jl
@@ -1,10 +1,3 @@
-# To-do:
-# * The length of the DFT vectors is generally not the same as the number of modes, so 
-#   I need to be careful about which is used as a type parameter
-# * two-body term should really be an interaction matrix, which does not play well with MomPotentialFunction;
-#   this may go back to how to deal with mixed term: u'(x-y) v'(x), where x and y may be of different components
-# * optimise STensor
-
 """
     trap_dft(M::Integer, v_ho::Real) -> w
 
@@ -23,7 +16,7 @@ where
 ```
 and ``\\mathrm{DFT}[…]_n`` is a discrete Fourier transform performed by `fft()[n%M + 1]`.
 """
-function trap_dft(M::Integer)
+function trap_dft(M::Int)
     # Set up potential like in Real1DEP
     is = range(-fld(M,2); length=M) # [-M÷2, M÷2) including left boundary
     js = shift_lattice(is) # shifted such that js[1] = 0
@@ -33,60 +26,61 @@ function trap_dft(M::Integer)
     for x in mom_potential
         @assert iszero(real(x)) || abs(imag(x) / real(x)) < sqrt(eps())
     end
-    # Make sure it's completely symmetric. It should be, but round-off errors can sometimes
-    # make it non-symmetric.
+    # enforce symmetry
     for i in 1:M÷2
         mom_potential[M - i + 1] = mom_potential[i + 1]
     end
-    return SVector{M}(real.(mom_potential))
+    # SVector not necessary
+    return SVector{M,Float64}(real.(mom_potential))
 end
 
 """
-    STensor(M; dft = trap_dft(M), pad = 1)
+    STensor(M, v)
 
 Compute the tensor
 ```math
     S_k = \\sum_{k'} k' \\, (k-k') \\tilde{v}(k') \\tilde{v}(k-k'),
 ```
 where ``\\tilde{v}(k)`` is the DFT of a harmonic trapping potential on a lattice of ``M`` sites.
-The values of `k` are in ``\\frac{2\\pi}{M\\alpha}[-M, M]``.
-The argument `pad` extends the sum over ``k'`` to include 
-``k' \\in \\frac{2\\pi}{M\\alpha}[-pad*M, pad*M]``.
+The values of `k` are in ``\\frac{2\\pi}{M\\alpha}[-M+1, M-1]``.
 """
 struct STensor{P}
     values::SVector{P,Float64}
 end
-function STensor(M; dft = trap_dft(M), pad = 1)
-    @assert length(dft) == M
-    P = 2M - 1              # no. of k values
+function STensor(M, v::SVector{Q,Float64}) where Q
+    P = 2M - 1
+    z = (Q + 1) ÷ 2M    # Q == 2zM - 1
+    
+    n_range = -M+1:M-1              # k values
+    np_range = -z*M+1:z*M-1         # k' values
 
     s = zeros(P)
-    np_range = -pad*M + 1:pad*M - 1
-    for i in 1:P
-        n = i - M
+    for n in n_range
         for np in np_range
-            s[i] += (2π/M)^2 * np * (n - np) * dft[1 + mod(np, M)] * dft[1 + mod(n - np, M)]
+            s[n + M] += (2π/M)^2 * np * (n - np) * v[1 + mod(np, Q)] * v[1 + mod(n - np, Q)]
         end
     end
+    # s = [sum((2π/M)^2 * np * (n - np) * v[1 + mod(n, Q)] * v[1 + mod(n - np, Q)] for np in np_range) for n in n_range]
     return STensor{P}(SVector{P}(s))
 end
-(s::STensor{P})(n::Int) where {P} = s.values[n + (P + 1) ÷ 2]
+(s::STensor{P})(n::Int) where {P} = s.values[n + (P + 1) ÷ 2]   # (P + 1) ÷ 2 == M for any M
 
 """
-    TCPotentialOneBody(M, c1, c2, pad)
+    TCPotentialOneBody(M, c1, c2, v)
 
 The function used in the one-body term of the transcorrelated harmonic trap potential.
 """
 struct TCPotentialOneBody{M,P,V}
-    corr_v::MomPotentialFunction{M,V}
+    corr_v::MomPotentialFunction{P,V}
     s::STensor{P}
     coeff2::V
 end
-function TCPotentialOneBody(M, c1, c2, pad)
-    P = 2M - isodd(M)           # range of k
-    dft = trap_dft(M)
-    corr_v = MomPotentialFunction(c1, dft)  # this might be too short? should have length P
-    s = STensor(M; dft, pad)
+function TCPotentialOneBody(M, c1, c2, v)
+    P = 2*M - 1             # range of k
+    # Q = 2*pad*M - 1         # range of k'
+    # dft = trap_dft(Q) / (pad*M)^3   # magic factor ≈ (zM)^3
+    corr_v = MomPotentialFunction(c1, SVector{P}(v[1:P]))
+    s = STensor(M, v)
     return TCPotentialOneBody{M,P,typeof(c2)}(corr_v, s, c2)
 end
 function (f::TCPotentialOneBody{M})(σ, p, q) where {M}
@@ -95,13 +89,13 @@ function (f::TCPotentialOneBody{M})(σ, p, q) where {M}
     p_pi = i_to_k(p, M) # NB: input momenta are in 1:M mode index form
     v_k = f.corr_v(σ, p, q)
     s_k = f.s(k)
-    c = f.coeff2(σ)
+    c2 = f.coeff2(σ)
 
-    return (k_pi^2 + 2p_pi * k_pi) * v_k + c * s_k
+    return (k_pi^2 + 2p_pi * k_pi) * v_k + c2 * s_k
 end
 
 """
-    TCPotentialTwoBody(M, cutoff, c)
+    TCPotentialTwoBody(M, v, cutoff, c)
 
 The function used in the two-body term of the transcorrelated harmonic trap potential.
 """
@@ -111,19 +105,20 @@ struct TCPotentialTwoBody{P,V}
     corr_v::MomPotentialFunction{P,ConstFunction{Int}}
     coeff::V
 end
-function TCPotentialTwoBody(M, cutoff::Int, c)
-    P = 2M - isodd(M)           # range of k and k'
-    dft = 2 .* trap_dft(P)  # factor of 2 is due to 1/M in definition of `momentum_space_harmonic_potential`
-    corr_v = MomPotentialFunction(ConstFunction{Int}(1), dft)   # this seems janky;
+function TCPotentialTwoBody(M, v, cutoff::Int, c3)
+    P = 2M - 1           # range of k and k'
+    corr_v = MomPotentialFunction(ConstFunction{Int}(1), SVector{P}(v[1:P]))
     corr_u = CorrelationFactor(M, cutoff; length=P)
-    TCPotentialTwoBody(M, corr_u, corr_v, c)
+    TCPotentialTwoBody(M, corr_u, corr_v, c3)
 end
 function (f::TCPotentialTwoBody)(σ, τ, s, r, q, p)
     M = f.num_modes
     kp = r - q
     k = s - p + kp
-    u = f.coeff(σ, τ)
-    return u * f.corr_v(1, k, 0) * n_to_k(kp, M) * f.corr_u(kp) * n_to_k(kp, M)
+    v_k = f.corr_v(σ, k, 0)
+    u_k = f.corr_u(kp)
+    c3 = f.coeff(σ, τ)
+    return c3 * v_k * n_to_k(kp, M) * u_k * n_to_k(kp, M)
 end
 
 """
@@ -134,13 +129,13 @@ Transcorrelated external potential with strength `v`.
 * `v` can be a real number or a vector of real numbers whose length matches the number of components in the
 Hamiltonian.
 * `b` is the width of the Jastrow factor relative to the trap length.
-* `pad` scales the number of terms to use in internal sums relative to the number of modes
-* `twobody` toggles the two body terms
+* `pad` scales the number of terms to use in internal sums relative to the number of modes, namely 
+    ``k' \\in \\frac{2\\pi}{M\\alpha}[-pad*M+1, pad*M-1]``.
+* `twobody` toggles the two body terms involving the interparticle interaction.
 
 Can only be used with momentum space Hamiltonians.
 """
 struct TranscorrelatedPotential{V} <: ExtensionPrototype
-    # v::V
     b::V
     pad::Int
     twobody::Bool
@@ -157,20 +152,23 @@ function initialize(tcp::TranscorrelatedPotential, ham)
         t = only(ham.t)
         pad = tcp.pad
 
-        # v = tcp.v   # trap strengths
         b = tcp.b   # Jastrow widths
 
         B = @. -1/4b^2                              # reconcile trap, interaction and Jastrow parameters
         c1 = parameter_column(address, B)           # coefficient of the quadratic and non-hermitian terms
         c2 = parameter_column(address, @. B^2 / t)  # coefficient of the derivative-squared term
+
+        # DFT
+        # P = 2*M - 1             # range of k
+        Q = 2*pad*M - 1         # range of k'
+        v_k = trap_dft(Q) / (pad*M)^3   # magic factor ≈ (zM)^3
         
-        term = FullOneBodyTerm(TCPotentialOneBody(M, c1, c2, pad))
-        # term += HarmonicPotential(v)
+        term = FullOneBodyTerm(TCPotentialOneBody(M, c1, c2, v_k))
         
         if tcp.twobody && u ≠ 0
             cutoff = ham isa Transcorrelated ? ham.cutoff : @warn "Using `TranscorrelatedPotential` with a bare Hamiltonian"
-            c3 = parameter_column(address, @. 2B * u/t)      # this actually needs to be an interaction_matrix
-            return term + FullTwoBodyTerm(TCPotentialTwoBody(M, cutoff, c3))
+            c3 = interaction_matrix(address, @. 2B * u/t)
+            return term + FullTwoBodyTerm(TCPotentialTwoBody(M, v_k, cutoff, c3))
         else
             return term
         end
